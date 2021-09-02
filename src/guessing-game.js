aa66ed35725e1cabd53e48c8ab4b5d287271a813
Короткий Владислав Владимирович <vladislav.korotky@becloud.by>
2021-09-02 22:26:56 +0300

    feat: end task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.mid
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.mid = Math.round((this.min + this.max) / 2);
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
